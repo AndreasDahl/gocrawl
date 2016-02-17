e7faf06eb4a46625138046d152746b082b07d547
Andreas Dahl <andreas@hrdahl.dk>
2016-02-17 18:11:24 +0100

    Get rid of tabs and start something new

--- src/main.go
+++ src/main.go
@@ -1,31 +1,40 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "io/ioutil"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
-
-
 func download(id int, c chan int) {
-    resp, err := http.Get(fmt.Sprintf("http://www.pathofexile.com/forum/view-thread/%d", id))
-    if err != nil {
-    	// handle error
-    }
-    body, err := ioutil.ReadAll(resp.Body)
-    ioutil.WriteFile(fmt.Sprintf("shops/%d.html", id), body, 0666)
-    c <- id
+	resp, err := http.Get(fmt.Sprintf("http://www.pathofexile.com/forum/view-thread/%d", id))
+	if err != nil {
+		// handle error
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	ioutil.WriteFile(fmt.Sprintf("shops/%d.html", id), body, 0666)
+	c <- id
+}
+
+func getShops() (ids [2]int) {
+	resp, err := http.Get("http://www.pathofexile.com/forum/view-forum/standard-trading-shops")
+	if err != nil {
+		// handle error
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	fmt.Printf("%s", body)
+	return [2]int{1, 2}
 }
 
 func main() {
-    const dl_count int = 5
-    c := make(chan int)
-    for i := 1588095; i > 1588095 - dl_count; i-- {
-        go download(i, c)
-    }
-
-    for i := 0..; i < dl_count; i++ {
-        fmt.Println("%d: done", <-c)
-    }
+	getShops()
+	// const dlCount int = 5
+	// c := make(chan int)
+	// for i := 1588095; i > 1588095-dlCount; i-- {
+	//     go download(i, c)
+	// }
+	//
+	// for i := 0; i < dlCount; i++ {
+	//     fmt.Printf("done: %d\n", <-c)
+	// }
 }
